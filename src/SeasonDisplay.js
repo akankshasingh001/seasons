6165baa0abfc96ec5651f1964fe8fe997679cc07
Akanksha Singh <singhakanksha001@gmail.com>
2022-04-06 16:34:53 -0400

    added SeasonDisplay file and Component

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SeasonDisplay = props => {
+  console.log(props.lat);
+  return <div>{props.lat}</div>;
+};
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { lat: null, errMessage: '' };
-  // }
   state = { lat: null, errMessage: '' };
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
@@ -18,7 +15,7 @@ class App extends React.Component {
       return <div>Error: {this.state.errMessage}</div>;
     }
     if (!this.state.errMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
     return <div>Loading...</div>;
   }
