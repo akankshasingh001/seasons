c6e0ee07ca69b8930a0e5f1604f4ae32267fd2b5
Akanksha Singh <singhakanksha001@gmail.com>
2022-04-06 11:05:08 -0400

    added constructor and initialize state

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
     window.navigator.geolocation.getCurrentPosition(
       position => console.log(position),
       err => console.log(err)
     );
+  }
+  render() {
     return <div>Latitude:</div>;
   }
 }
