e1dbe523f97477335a7bd1c980f6268299a7e1bb
Akanksha Singh <singhakanksha001@gmail.com>
2022-04-06 17:13:54 -0400

    create seasonConfig to avoid code repition

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+const seasonConfig = {
+  Summer: { text: "Let's hit the beach!!!", iconName: 'sun' },
+  Winter: { text: "Burr,It's Cold!!!" }
+};
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? 'Summer' : 'Winter';
@@ -9,8 +14,14 @@ const getSeason = (lat, month) => {
 };
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const text = season === 'Winter' ? 'Burr,It is chilly' : 'Lets hit the beach';
-  return <div>{text}</div>;
+  const { text, iconName } = seasonConfig[season];
+  return (
+    <div>
+      <i className={`${iconName}icon`} />
+      <h1>{text}</h1>
+      <i className={`${iconName}icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
