dff65b07ace646cd5b2eb50af4f08065f4917ad5
Akanksha Singh <singhakanksha001@gmail.com>
2022-04-06 16:47:19 -0400

    created method to get season getSeason()

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'Summer' : 'Winter';
+  } else {
+    return lat > 0 ? 'Winter' : 'Summer';
+  }
+};
 const SeasonDisplay = props => {
-  console.log(props.lat);
-  return <div>{props.lat}</div>;
+  const season = getSeason(props.lat, new Date().getMonth());
+  console.log(season);
+  return <div>Season Display</div>;
 };
 
 export default SeasonDisplay;
